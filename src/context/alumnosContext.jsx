92e5c6d5a81149dccc5c8f9176d0b2cdbe9d219a
Adlai CG <42752044+eriadlai@users.noreply.github.com>
2022-11-18 13:37:23 -0800

    actualizacion

---
+++ src/context/alumnosContext.jsx
@@ -0,0 +1,27 @@
+import { createContext, useState, useEffect, useContext } from "react";
+import { BaseApiUrl } from "../api/ApiUrl";
+
+const AlumnosContext = createContext({});
+
+const AlumnoContextProvider = ({ children }) => {
+  const [alumno, setAlumno] = useState([]);
+
+  useEffect(() => {
+    BaseApiUrl.get("/Alumno").then((alumno) =>
+      setAlumno(alumno.data[0])
+    );
+  }, []);
+
+
+  return (
+    <AlumnosContext.Provider
+      value={{alumno}}
+    >
+      {children}
+    </AlumnosContext.Provider>
+  );
+};
+
+export default AlumnoContextProvider;
+
+export const useInventarioContext = () => useContext(InventarioContext);
--- src/utilities/Table.jsx
+++ src/utilities/Table.jsx
@@ -1,49 +1,54 @@
 import * as React from 'react';
-import { DataGrid } from '@mui/x-data-grid';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
-const columns = [
-  { field: 'id', headerName: 'ID', width: 70 },
-  { field: 'nombre', headerName: 'Nombre', width: 130 },
-  { field: 'apellido', headerName: 'Apellido', width: 130 },
-  {
-    field: 'matricula',
-    headerName: 'Matricula',
-    type: 'number',
-    width: 90,
-  },
-  {
-    field: 'semestre',
-    headerName: 'Semestre',
-    description: 'This column has a value getter and is not sortable.',
-    sortable: false,
-    width: 160,
-  },
+function createData(name, calories, fat, carbs, protein) {
+  return { name, calories, fat, carbs, protein };
+}
+
+const rows = [
+  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+  createData('Eclair', 262, 16.0, 24, 6.0),
+  createData('Cupcake', 305, 3.7, 67, 4.3),
+  createData('Gingerbread', 356, 16.0, 49, 3.9),
 ];
 
-const rows = []
-
-export default function DataTable() {
-  const urlAlumnos="https://3ls4od9od7.execute-api.us-west-1.amazonaws.com/dev/Alumno";
-  const [oData, setData] = React.useState();
-
-    React.useEffect(() => {
-        fetch(urlAlumnos)
-            .then(response => response.json())
-            .then(data => 
-              data[0].map((element)=>(
-                console.log(element)
-              )));
-    }, []);
-    console.log(oData)
+export default function BasicTable() {
   return (
-    <div style={{ height: 400, width: '100%' }}>
-      <DataGrid
-        rows={rows}
-        columns={columns}
-        pageSize={5}
-        rowsPerPageOptions={[5]}
-        checkboxSelection
-      />
-    </div>
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Dessert (100g serving)</TableCell>
+            <TableCell align="right">Calories</TableCell>
+            <TableCell align="right">Fat&nbsp;(g)</TableCell>
+            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
+            <TableCell align="right">Protein&nbsp;(g)</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow
+              key={row.name}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {row.name}
+              </TableCell>
+              <TableCell align="right">{row.calories}</TableCell>
+              <TableCell align="right">{row.fat}</TableCell>
+              <TableCell align="right">{row.carbs}</TableCell>
+              <TableCell align="right">{row.protein}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
   );
 }
